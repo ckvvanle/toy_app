7d1e12b1be07af0980f0854731567cf372af2b62
ckvvanle <phamvanle10dt2.bkdn@gmail.com>
2018-07-22 00:31:18 +0700

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world! kaka"
+    render html: "hello, world! cr7"
   end
 end
